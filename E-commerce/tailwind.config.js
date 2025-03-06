5549ff58ab68b1629ff7396933f99b590df45719
KondaVinay28 <kondavinay555@gmail.com>
2025-03-06 20:02:52 +0530

    fixed pop-up-offer

---
+++ E-commerce/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["*.html", "*.js", "*.jsx", "./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
