6d27c81b70a9ecac7ec18c2648cfc99b7b1eb895
KondaVinay28 <kondavinay555@gmail.com>
2025-03-07 22:29:50 +0530

    NavBar

--- E-commerce/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["*.html", "*.js", "*.jsx", "./src/**/*.{html,js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
